ded89f09fbab38a0a4c09c02a5c71061bec2d141
Martin Koldovsky <mkoldov@gmail.com>
2026-06-11 12:35:41 +0000

    prefix

--- cpman/main.tf
+++ cpman/main.tf
@@ -1,9 +1,9 @@
 locals {
   envId             = var.envId
-  rg                = "automagic-management-${local.envId}"
+  rg                = "labvmmsup-management-${local.envId}"
   location          = "westeurope"
   mgmt_name         = "cpman-${local.envId}"
-  vnet_name         = "automagic-management-vnet-${local.envId}"
+  vnet_name         = "labvmmsup-management-vnet-${local.envId}"
   vnet_address      = "10.0.0.0/16"
   management_subnet = "10.0.1.0/24"
   vm_size           = "Standard_D3_v2"
